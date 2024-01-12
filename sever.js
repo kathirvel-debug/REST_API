9d9bcb01cc2e85c127ba42ff9414a29ee21b2405
kathirvel-debug <68596240+kathirvel-debug@users.noreply.github.com>
2024-01-12 18:39:47 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -2,7 +2,7 @@ import express from 'express'
 import router from './src/router.js';
 import connectUsingMongoose from './config/mangoose.js'
 const sever=express();
-var postmanUrl:'https://www.postman.com/postman/workspace/postman-public-workspace/request/create?requestId=3cd701b5-3c3f-4293-9fde-a2456a737784'
+var postmanUrl='https://www.postman.com/postman/workspace/postman-public-workspace/request/create?requestId=3cd701b5-3c3f-4293-9fde-a2456a737784'
 sever.use(express.json())
 sever.use('/api/ecom',router);
 sever.get('/',(req,res)=>{
