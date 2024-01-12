268049193b79eaac899cfdb078ec4349739ed8ce
kathirvel-debug <68596240+kathirvel-debug@users.noreply.github.com>
2024-01-12 15:21:20 +0530

    Update mangoose.js

--- config/mangoose.js
+++ config/mangoose.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 const url = 'mongodb://localhost/skilltest';
 const connectUsingMongoose = async()=>{
-    try{
-        await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Mongodb connected using mongoose");
-    }catch(err){
-        console.log("Error while connecting to db");
-        console.log(err);
-    }
+   try {
+        await mongoose.connect('mongodb://localhost/skilltest');
+        console.log('Connected to MongoDB');
+      } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+      }
 }
 
 export default connectUsingMongoose
